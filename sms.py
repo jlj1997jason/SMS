4dda8758bed00cf35a94725853a0eebe0c6ee923
jlj1997jason <30722770+jlj1997jason@users.noreply.github.com>
2022-09-25 15:07:33 +0800

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,100 @@
+import os
+import time
+from termcolor import colored
+import requests
+import sys
+
+
+def main():
+    def logo():
+        print(colored(
+            """
+    @@@@@@@@@@@@@@@@@@@@@@@@@@**********@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@*****************************@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@**************************************@@@@@@@@@@@@
+    @@@@@@**************@@@@@@@@@@@@@@@@@@@@@@@**************@@@@@
+    @@@***    *****@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*****    ***@@
+    @@***      **@@@@@@@@@@*****************@@@@@@@@@@**      **@@
+    @@@****  ***@@@@@@***************************@@@@@@***  ***@@@
+    @@@@@@@@@@@@*************@@@@@@@@@@@@@*************@@@@@@@@@@@
+    @@@@@@@@@@***    ***@@@@@@@@@@@@@@@@@@@@@@@***    **@@@@@@@@@@
+    @@@@@@@@@@@***  ***@@@@@@@***********@@@@@@@**   ***@@@@@@@@@@
+    @@@@@@@@@@@@@@*@@@@@***********************@@@@*@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@**    *****@@@@@*****    **@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@***  ***@@@@@@@@@@@***  **@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@*******@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@*********@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@********@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@**@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    **@@@**@@@@@@@@@@@@@@@@@@**@@@@@@*@@@@@*@@@@@@@@@@@@@@@@@*@@@@
+    **@@@**@*@@@*@*****@@******@@*****@@@@****@*****@@*****@@*****
+    **@@@**@@*@*@@*@@@**@*@@@**@**@@@*@@@@@**@@*@***@@*@@@@@@*@@@*
+    **@@@**@@@**@@*@***@@*@@@**@****@*@@@@@***@*****@@*****@@*@@@*
+    @@@@@@@@**@@@@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    
+    
+    We are not responsible of any illegal action
+    """, 'blue'))
+
+    logo()
+
+    time.sleep(1)
+
+    print('1: Send sms')
+    print('2: Help!')
+
+    choices = input("\nPlease choice: ")
+
+    def SMS():
+        os.system('clear')
+        time.sleep(1)
+        logo()
+        time.sleep(1)
+        smsnumb = input("Input the number of the target: ")
+        message = input("Please input the message: ")
+
+        resp = requests.post('https://textbelt.com/text', {
+            'phone': f'{smsnumb}',
+            'message': f'{message}',
+            'key': 'textbelt',
+        })
+
+        print(resp.json())
+        time.sleep(3)
+        return main()
+
+    if choices == "1":
+        SMS()
+
+    elif choices == "2":
+        os.system("clear")
+        time.sleep(1)
+        logo()
+        time.sleep(1)
+        lines = "=========================================================================================="
+        print(
+            "Your phone number was not provided in E.164 format: you need to input the country code ex.+91"
+        )
+        print(lines)
+        print(
+            "Only one test text message is allowed per day: you can't send more than one message per day, however you could use vpn"
+        )
+        print(lines)
+        print(
+            "Else: please contact hyprid tech in github or youtube"
+        )
+        print(lines)
+        print(
+            ""
+        )
+
+    elif choices == "-1":
+        os.system("clear")
+        time.sleep(1)
+        logo()
+        time.sleep(1)
+        return
+
+
+main()
